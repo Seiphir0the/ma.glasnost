56307e430e637143320b8f0efa6ba23e675d5610
Seiphir0the <moukhliss.amine@gmail.com>
2013-03-13 18:36:07 +0000

    refactoring

--- clojure-start/src/clojure_start/core.clj
+++ clojure-start/src/clojure_start/core.clj
@@ -5,17 +5,3 @@
   [x]
   (println x "Hello, World!"))
 
-;this function uses if (expression) (true return result 1) (false return result 2)
-(defn isOld [age]
-  "this function return whether this person is old or not"
-  (if (< age 18)
-    "this person is young"
-    "this person is old"))
-
-(defn sayHi [name]
-  (println "Hello " name))
-
-;forms
-;the IF form
-(defn isAString [ argument ]
- (if (= String (class argument)) " a string your good to go" (str " naaaah, not a string, it's a " (class argument))))
